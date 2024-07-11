97a7c1e7a6a24f95495ddd2276d94b436670e839
Ivan <RogerpIvan@yandex.ru>
2024-07-11 19:03:49 +0300

    virtual dom fix

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-import {IVirtualElement} from "./virtual-dom/virtual-dom.types";
-import {createElementFromVNode, mount} from "./virtual-dom/virtual-dom";
+import {VDom} from "./virtual-dom/virtual-dom.js";
 
-const virtualDom: IVirtualElement = {
-  tag: 'div',
-  props: { 'id': '89' },
-  content: ['text']
-}
+const vNode = VDom.createVElement('div', {id: 'app'}, ['text'])
+const dom = VDom.createElementFromVNode(vNode) as HTMLElement
+const target = document.getElementById('app')
+const vNode1 = VDom.createVElement('span', {id: 'app'}, ['Text'])
 
-const dom = createElementFromVNode(virtualDom)
-const targer = document.getElementById('app')
-if(targer) mount(dom, targer)
+if(target) VDom.mount(dom, target)
+
+VDom.upDateVNode(dom, vNode, vNode1)
--- src/virtual-dom/virtual-dom.ts
+++ src/virtual-dom/virtual-dom.ts
@@ -1,12 +1,13 @@
-import type { IVirtualNode } from "./virtual-dom.types"
+import type {IVirtualElement, IVirtualNode, IVirtualNodeProps} from "./virtual-dom.types.js"
 
-// const createVElement = (
-//                         tag: string,
-//                         props: IVirtualNodeProps = {},
-//                         content:  IVirtualNode[] = []
-// ): IVirtualElement => {
-//   return { tag, props, content }
-// }
+export namespace VDom{
+  export const createVElement = (
+                        tag: string,
+                        props: IVirtualNodeProps = {},
+                        content:  IVirtualNode[] = []
+  ): IVirtualElement => {
+    return { tag, props, content }
+  }
 
   export const createElementFromVNode = (vNode: IVirtualNode | string) => {
     if (typeof vNode === 'string') {
@@ -14,7 +15,6 @@ import type { IVirtualNode } from "./virtual-dom.types"
     }
 
     const {tag, props = [], content = []} = vNode
-
     const element = document.createElement(tag)
 
     for (const [key, value] of Object.entries(props)) {
@@ -32,3 +32,24 @@ import type { IVirtualNode } from "./virtual-dom.types"
     target.replaceWith(node)
     return node
   }
+
+  export const upDateVNode = (container: Element, vNode: IVirtualNode, newNode: IVirtualNode) => {
+    if(typeof vNode === 'string' || typeof newNode === 'string'){
+      if(vNode !== newNode){
+        const nextNode = createElementFromVNode(newNode)
+        container.replaceWith(nextNode)
+        return nextNode
+      }
+
+      return container
+    }
+
+    if(vNode.tag !== newNode.tag){
+      const nextNode = createElementFromVNode(newNode)
+      container.replaceWith(nextNode)
+      return nextNode
+    }
+
+    return container
+  }
+}
